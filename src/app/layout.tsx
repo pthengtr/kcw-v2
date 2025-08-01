df1a660998a25af3ff49944cb8a0936706041534
pthengtr <pthengtr@gmail.com>
2025-08-02 02:42:12 +0700

    global loading spinner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Prompt } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
+import GlobalLoading from "@/components/common/GlobalLoading";
 
 const prompt = Prompt({
   weight: ["400", "700"],
@@ -23,6 +24,7 @@ export default function Layout({
   return (
     <html lang="en" className="h-full">
       <body className={`${prompt.className} antialiased h-full`}>
+        <GlobalLoading />
         {children}
         <Toaster richColors expand={true} />
       </body>
