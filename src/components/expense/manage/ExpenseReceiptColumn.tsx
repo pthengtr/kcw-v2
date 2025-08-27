c9fa3e472a5fa961b3bb9b5ce93fbe6c6d065511
pthengtr <pthengtr@gmail.com>
2025-08-27 11:37:45 +0800

    remove unused import

--- src/components/expense/manage/ExpenseReceiptColumn.tsx
+++ src/components/expense/manage/ExpenseReceiptColumn.tsx
@@ -8,7 +8,6 @@ import {
   HeaderContext,
   Row,
 } from "@tanstack/react-table";
-import { Check } from "lucide-react";
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
